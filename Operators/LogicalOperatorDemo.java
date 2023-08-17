650aba5f6fe1222f4e5c79f770738d6ef9c9e21e
Nernika Boya Raju <88661224+nenrikaraju123@users.noreply.github.com>
2023-08-17 21:36:03 +0530

    Operators
    program on operators

---
+++ Operators/LogicalOperatorDemo.java
@@ -0,0 +1,28 @@
+class LogicalOperatorDemo
+{
+	public static void main(String[] args)
+	{
+		int p=10;q=20;
+		boolean a=true,b=false,c=true;
+		int sum=p+q'
+		int sub=p-q;
+		int mul=p*q;
+		float dv=p/q;
+		int mu=p%q; 
+		System.out.println("sum="+sum);
+		System.out.println("sub="+sub);
+		System.out.println("product="+mul);
+		System.out.println("divison="+dv);
+		System.out.println("rem="+mu);
+		System.out.println(p>q);
+		System.out.println(p<q);
+		System.out.println(p<=q);
+		System.out.println(a && a||b);
+		System.out.println(b && a&&a);
+		System.out.println(a || b||c);
+		System.out.println(a || b);
+	}
+}
+
+		
+		
